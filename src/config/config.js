6a4ee0a167b6a997d9fe40121b78fe0ea971db0e
Thanakorn Khumsub <ks.thanakorn@gmail.com>
2021-05-14 23:15:53 +0700

    [Fix] Cor

--- src/config/config.js
+++ src/config/config.js
@@ -24,7 +24,6 @@ if (error) {
 module.exports = {
     env: envVars.NODE_ENV,
     port: envVars.PORT,
-    cor_origin: envVars.COR_ORIGIN,
     mongoose: {
       url: envVars.MONGODB_URL + (envVars.NODE_ENV === 'test' ? '-test' : ''),
       options: {
